ddae1201776cf72ead63dd4156b5b2fe01bd27bb
Allison Gottlieb <allison.m.gottlieb@gmail.com>
2021-01-27 21:10:18 -0500

    JS working on click

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
-const activeState = document.getElementById("active-state");
-const share = document.querySelector(".btn");
+const share = document.querySelector('.btn');
+const activeState = document.querySelector('.active-state');
 
 const toggle = () => {
-    activeState.classList.toggle("visible");
+    activeState.classList.toggle('visible');
     console.log(1);
 };
 
-share.addEventListener("click", toggle);
+share.addEventListener('click', toggle);
